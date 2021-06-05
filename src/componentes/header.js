8e3f859db7a0a3ef7faaaf9b7eb8d9299417ba95
enicio <jessandro42@gmail.com>
2021-06-05 18:33:56 -0300

    Primeira versão do readme

---
+++ src/componentes/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import logo from '../assets/logo.png';
+import Delivery_motocycle from '../assets/Delivery_motocycle.png';
+import isopen from '../assets/isopen.png';
+
+import './header.scss';
+
+function Header() {
+  return(
+    <header className="header">
+      <img className="header__logo" src={ logo } alt="logo"/>
+      <div className="header__information__container">
+          <div className="header__information__container__title">
+            <h3>Pet Friends Acessories</h3>
+            <img className="header__information__container__title__isopen" src={ isopen } alt="open"/>
+          </div>
+          <h5>Avenida Rio Grande do Sul, 1520, Estados | 58030-021 | João Pessoa - PB</h5>
+          <div className="header__information__container__botton">
+            <div className="header__information__container__botton__delivery">
+              <img src={ Delivery_motocycle } alt="motocicle"/>
+              <h6>35m - 1h:40m</h6>
+            </div>
+            <div className="header__information__container__botton__entrega">
+              <h6> Entrega:</h6>
+              <h6>À partir de R$ 3,00</h6>
+            </div>
+          </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
